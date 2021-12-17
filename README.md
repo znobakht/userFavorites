7dc55cce5ec6fb7933868cf4650544c8753334e6
zakie nobakht <sAk20j8gH>
2021-12-17 19:25:14 +0330

    email and password are required.
    adding profile interface.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # readme
 # gitIgnore
     node_moduls and package-lock are unnecessary.
-    
+# profile Model
+    email and password are required to secure login and etc.
+    adding interface to be sure about format of inputs.
--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -2,10 +2,20 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
+interface profileInterface {
+  name?: string;
+  nickname?: string;
+  email: string;
+  password: string;
+  capital?: number;
+  divisa?: string;
+  prefered_cryptocurrency?: string;
+}
 const schema = new Schema({
   name: String,
   nickname: String,
-  email: String,
+  email: {type: String, required: true},
+  password: {type: String, required: true},
   capital: Number,
   divisa: String,
   prefered_cryptocurrency: String,
