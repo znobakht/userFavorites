24d11ffa83b942c33fbf2ecb941fd6fba2c51dbe
zakie nobakht <sAk20j8gH>
2021-12-19 15:46:37 +0330

    using try catch bloch

--- src/routes/profile.router.ts
+++ src/routes/profile.router.ts
@@ -6,19 +6,34 @@ import _ from "lodash";
 export let router = express.Router();
 
 router.post("/api/profile", async (req, res) => {
-  const { email, password, name, nickname } = req.body;
+  try {
+    const { email, password, name, nickname } = req.body;
 
-  let profile = await Profile.findOne({ email });
+    let profile = await Profile.findOne({ email });
 
-  if(profile){
-    res.status(403).json({msg: 'user already exists'});
+    if (profile) {
+      res.status(403).json({ msg: "user already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const newPass = await bcrypt.hash(password, salt);
+    profile = await Profile.create({
+      email,
+      name,
+      nickname,
+      password: newPass,
+    });
+    const userForSend: any = _.pick(profile, [
+      "_id",
+      "name",
+      "email",
+      "nickname",
+    ]);
+
+    res.status(200).json(userForSend);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ msg: err.message });
   }
-  const salt = await bcrypt.genSalt(10);
-	const newPass = await bcrypt.hash(password, salt);
-  profile = await Profile.create({email, name, nickname, password: newPass});
-  const userForSend: any = _.pick(profile, ["_id", "name", "email", "nickname"]);
-
-  res.status(200).json(userForSend);
 });
 router.get("/api/profile", async (req, res) => {
   var profile = await Profile.find().lean();
