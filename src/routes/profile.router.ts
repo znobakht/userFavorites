50a4e273c3fd3f2dd33a99c0dd0ba1fe910472a4
zakie nobakht <sAk20j8gH>
2021-12-20 14:33:42 +0330

    auth process is done in other router

--- src/routes/profile.router.ts
+++ src/routes/profile.router.ts
@@ -1,42 +1,11 @@
 import express from "express";
 import { Profile } from "../models/Profile";
-import bcrypt from "bcryptjs";
-import _ from "lodash";
 
-export let router = express.Router();
+export var router = express.Router();
 
-router.post("/api/profile", async (req, res) => {
-  try {
-    const { email, password, name, nickname } = req.body;
-
-    let profile = await Profile.findOne({ email });
-
-    if (profile) {
-      res.status(403).json({ msg: "user already exists" });
-    }
-    const salt = await bcrypt.genSalt(10);
-    const newPass = await bcrypt.hash(password, salt);
-    profile = await Profile.create({
-      email,
-      name,
-      nickname,
-      password: newPass,
-    });
-    const userForSend: any = _.pick(profile, [
-      "_id",
-      "name",
-      "email",
-      "nickname",
-    ]);
-
-    res.status(200).json(userForSend);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ msg: err.message });
-  }
-});
 router.get("/api/profile", async (req, res) => {
   var profile = await Profile.find().lean();
   console.log(profile);
   res.json({ profile });
 });
+
